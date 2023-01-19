008f22902b9348d5c039723331370bcdd67b7333
Daria Kolodzey <dkolodze@student.codam.nl>
2023-01-19 21:28:33 +0100

    Add RAM stats

--- monitoring.sh
+++ monitoring.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Output fancy banner
-# cat /sbin/cronjobs/banner.txt;
+cat /sbin/cronjobs/banner.txt;
 
 # Architecture
 echo "#Architecture: $(uname -a)";
@@ -13,3 +13,12 @@ echo "#Architecture: $(uname -a)";
 # Processors
 echo "#CPU physical : $(cat /proc/cpuinfo | grep '^physical id' | sort | uniq | wc -l)";
 echo "#vCPU : $(cat /proc/cpuinfo | grep '^processor' | wc -l)";
+
+# RAM
+total_RAM_MB="$(free -m | sed -n 2p | awk '{print $2}')";
+# echo $total_RAM_MB
+used_RAM_MB="$(free -m | sed -n 2p | awk '{print $3}')";
+# echo $used_RAM_MB
+percentage_used_RAM="$(awk -v used=$used_RAM_MB -v total=$total_RAM_MB 'BEGIN {printf "%.2f", used / total * 100}')";
+# echo $persentage_used_RAM
+echo "#Memory Usage: $used_RAM_MB/$total_RAM_MB ($percentage_used_RAM%)";
